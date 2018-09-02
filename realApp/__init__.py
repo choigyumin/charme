4ecb8b1a98af1af324a9e05f84a6fa1b91483949
choigyumin <choigyumin@gmail.com>
2018-09-02 12:56:41 +0900

    button type init test

--- realApp/__init__.py
+++ realApp/__init__.py
@@ -6,23 +6,47 @@ app = Flask(__name__)
 
 @app.route('/keyboard', methods=['GET'])
 def keyboard():
-  return jsonify({'type': 'button',\
-                  'buttons': ["견적문의", "후기남기기", "별점주기"]
-                  })
+  # user input keyboard !
+  # type of text (default)
+  # and button... 
+  server_to_user_data = {
+    'type': 'buttons',
+    'buttons': ["견적문의", "후기남기기", "별점주기"]
+    }
+  return jsonify(server_to_user_data)
 
 @app.route('/message', methods=['POST'])
 def message():
 
-  #content = request.json['content']
-  #response = content
+  data_from_user = request.get_json()
+  content = data_from_user['content']
 
-  return jsonify({'type': 'button',\
-                  'buttons': ["견적문의", "후기남기기", "별점주기"]
-                  })
+  # 견적문의
+  if content == u"견적문의":
+    data_to_user = {
+      "message": {
+        "buttons": ["예식 날짜, 시간", \
+                    "예식 장소"]
+      }
+    }
+  elif content == u"후기남기기":
+    data_to_user = {
+      "message": {
+        "text": "후기를 남겨주시면 \
+        저희에게 큰 도움이 됩니다."
+      }
+    }
+  else: # 별점주기
+    data_to_user = {
+      "message": {
+        "buttons": ["매우만족", "만족", "보통", "실망"]
+      }
+    }
 
-# TODO: have to change for counselings...
+
+  return jsonify(data_to_user)
+
 # 견적문의: 견적 양식 텍스트 return,
 # 후기남기기: 후기 양식 텍스트 return..
 # 여기에 별점 버튼도 포함 !
-# 별점주기: 별점 버튼 5개 return.
 
